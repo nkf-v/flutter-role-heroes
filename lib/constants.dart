41aadb42dd0e9e29d270f8228378fcf8135cfc1a
Vasiliy Nikiforov <nkf@feip.co>
2020-12-29 19:03:57 +1000

    Create constants for theme

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const gTextColor = Color(0xff535353);
+const gTextLightColor = Color(0xffacacac);
+const gBorderColor = Color(0xffacacac);
+
+const gDefaultPadding = 20.0;
+const gDefaultMargin = 20.0;
+
+const minSizeIconButtons = 7.0;
