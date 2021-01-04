fc802622dff5aac7053741ba0f894a2b2e62f823
Vasiliy Nikiforov <nkf@feip.co>
2021-01-05 00:00:26 +1000

    Create attribute model

---
+++ lib/models/attribute.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:role_heroes/utils/value_types.dart';
+
+import 'category.dart';
+
+class Attribute {
+  int id;
+  String name;
+
+  BaseValueType type;
+  String _value;
+  Category category;
+
+  Attribute({
+    @required this.id,
+    @required this.name,
+    @required this.type,
+    @required dynamic value,
+    @required this.category,
+  }) {
+    _value = value.toString();
+  }
+
+  get value => type.convertValue(_value);
+
+  set value(dynamic value) {
+    _value = value.toString();
+  }
+}
