1d9b8139f272bc39fa61fa4a156857d635111e5a
Vasiliy Nikiforov <nkf@feip.co>
2021-01-05 00:03:21 +1000

    Create light theme

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class LightTheme {
+  static build(context) {
+    return ThemeData(
+      primaryColor: Colors.white,
+      accentColor: Colors.green,
+      textTheme: TextTheme(
+        bodyText1: TextStyle(color: gTextColor, fontSize: 16.0),
+        bodyText2: TextStyle(color: gTextColor, fontSize: 18.0),
+
+        headline1: TextStyle(color: gTextColor, fontSize: 28.0, fontWeight: FontWeight.bold),
+        headline2: TextStyle(color: gTextColor, fontSize: 24.0, fontWeight: FontWeight.bold),
+        headline3: TextStyle(color: gTextColor, fontSize: 20.0, fontWeight: FontWeight.bold),
+      )
+//      visualDensity: VisualDensity.standard,
+    );
+  }
+}
