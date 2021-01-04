842505e68f1b1251123377f46c3811e013291b31
Vasiliy Nikiforov <nkf@feip.co>
2021-01-05 00:04:03 +1000

    Create hero detail screen builder

---
+++ lib/utils/builders/hero_detail_screen_builder.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:role_heroes/components/characteristics_builder.dart';
+import 'package:role_heroes/components/field.dart';
+import 'package:role_heroes/models/attribute.dart';
+import 'package:role_heroes/models/category.dart';
+import 'package:role_heroes/models/user_hero.dart';
+import 'package:role_heroes/widgets/category_tab.dart';
+
+abstract class IHeroDetailScreenBuilder {
+  void reset();
+  void buildMainFieldsCategory(UserHero hero);
+  void buildCharacteristicsCategory(UserHero hero);
+  void buildCategories(UserHero hero);
+}
+
+class HeroDetailScreenBuilder extends IHeroDetailScreenBuilder {
+  List<CategoryTab> _categories;
+  List<Widget> _views;
+
+  @override
+  void reset() {
+    _categories = [];
+    _views = [];
+  }
+
+  @override
+  void buildMainFieldsCategory(UserHero hero) {
+    _categories.add(
+      CategoryTab(
+        category: Category(
+            name: 'Base fields'
+        )
+      )
+    );
+    _views.add(
+      Column(
+        children: [
+          Field(name: 'Name', value: hero.name),
+          Field(name: 'Note', value: hero.note),
+        ],
+      )
+    );
+  }
+
+  @override
+  void buildCharacteristicsCategory(UserHero hero) {
+    _categories.add(
+        CategoryTab(
+            category: Category(
+                name: 'Characteristics'
+            )
+        )
+    );
+    _views.add(
+      CharacteristicsBuilder(characteristics: hero.characteristics),
+    );
+  }
+
+  @override
+  void buildCategories(UserHero hero) {
+    Map<Category, List<Attribute>> categoriesAttributes = Map.fromIterable(hero.attributes, key: (attribute) => attribute.category, value: (attribute) => []);
+    hero.attributes.forEach((Attribute attribute) {
+      categoriesAttributes[attribute.category].add(attribute);
+    });
+    categoriesAttributes.forEach((Category category, List<Attribute> attributes) {
+      _categories.add(CategoryTab(category: category));
+      _views.add(ListView.builder(
+          itemCount: attributes.length,
+          itemBuilder: (context, index) {
+            Attribute attribute = attributes.elementAt(index);
+            return Field(name: attribute.name, value: attribute.value);
+          }
+      )
+      );
+    });
+  }
+
+  List<CategoryTab> getCategories() {
+    return _categories;
+  }
+
+  List<Widget> getViews() {
+    return _views;
+  }
+}
