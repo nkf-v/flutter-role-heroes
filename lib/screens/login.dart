eb774059b1df804a17bcbe23f338ffb97bc74941
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-01-10 18:30:00 +1000

    Implement login screen logic

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:role_heroes/components/flushbar.dart';
 import 'package:role_heroes/constants.dart';
+import 'package:role_heroes/controllers/AuthController.dart';
+import 'package:role_heroes/screens/game_list.dart';
 import 'package:role_heroes/screens/register.dart';
+
+class LoginFormValues {
+  String login;
+  String password;
+}
 
 class LoginScreen extends StatelessWidget {
   static final routeName = '/';
 
   final _formKey = GlobalKey<FormState>();
+  final LoginFormValues formValues = LoginFormValues();
+  // TODO Change method get controller
+  final AuthController controller = AuthController();
 
-  void submitLoginForm() {
+  void login(BuildContext context) async {
+    MainFlushbar processFlushbar = MainFlushbar(message: 'Process', showProgressIndicator: true)..show(context);
+    var result = await controller.login(formValues.login, formValues.password);
+    processFlushbar.dismiss();
+
+    if (result is bool && result) {
+      MainFlushbar(
+        message: 'Log in success',
+        statusColor: Colors.green,
+        duration: Duration(seconds: 2),
+      ).show(context);
+      Navigator.of(context).pushReplacementNamed(GameScreen.routeName);
+    }
+    else {
+      MainFlushbar(
+        message: result,
+        statusColor: Colors.red,
+        duration: Duration(seconds: 2),
+      ).show(context);
+    }
+  }
+
+  void submitLoginForm(BuildContext context) async {
     _formKey.currentState.save();
     if (_formKey.currentState.validate()) {
-      // TODO login by api method
+      login(context);
     }
   }
 
@@ -38,6 +71,13 @@ class LoginScreen extends StatelessWidget {
                     margin: const EdgeInsets.only(bottom: gDefaultMargin),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
+                      validator: (String value) {
+                        String result;
+                        if (value.length < 3)
+                          result = 'Login length is less 3 characters';
+                        return result;
+                      },
+                      onSaved: (String value) => formValues.login = value,
                       decoration: InputDecoration(
                         labelText: 'Login',
                         hintText: 'Enter login',
@@ -50,7 +90,15 @@ class LoginScreen extends StatelessWidget {
                   Container(
                     margin: const EdgeInsets.only(bottom: gDefaultMargin),
                     child: TextFormField(
+                      obscureText: true,
                       keyboardType: TextInputType.text,
+                      validator: (String value) {
+                        String result;
+                        if (value.length < 6)
+                          result = 'Password length is less 6 characters';
+                        return result;
+                      },
+                      onSaved: (String value) => formValues.password = value,
                       decoration: InputDecoration(
                         labelText: 'Password',
                         hintText: 'Enter password',
@@ -64,7 +112,7 @@ class LoginScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         ElevatedButton(
-                          onPressed: () {},
+                          onPressed: () => submitLoginForm(context),
                           child: Text('Log in'),
                         ),
                         RaisedButton(
