d7b6b2ae0e1109f6152aaeaa09db82e5e27d74e0
Vasiliy Nikiforov <kaktusv6@gmail.com>
2021-01-10 18:24:48 +1000

    Add pakcage for create snackbar without scafold widget

---
+++ lib/components/flushbar.dart
@@ -0,0 +1,18 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+
+class MainFlushbar extends Flushbar
+{
+  MainFlushbar({
+    @required message,
+    bool showProgressIndicator = false,
+    Color statusColor,
+    Duration duration,
+  }) : super(
+    message: message,
+    showProgressIndicator: showProgressIndicator,
+    margin: EdgeInsets.all(8),
+    leftBarIndicatorColor: statusColor,
+    duration: duration,
+  );
+}
